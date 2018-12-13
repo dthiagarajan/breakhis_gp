fix roc calculation stuff

roc_analysis.py
@@ -1,6 +1,6 @@
 import argparse
-import os
 import random
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.parallel
@@ -9,13 +9,11 @@ import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
 import torch.nn.functional as F
-import numpy as np
-import sklearn
 import time
 import math
-import matplotlib.pyplot as plt
 from dataloader import BreaKHis_v1Lister as lister
 from dataloader import BreaKHis_v1Loader as DA
+from matplotlib import pyplot as plt
 from model import *
 from resnet import *
 from sklearn.metrics import *
@@ -53,16 +51,31 @@ torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
-images, all_params, labels = lister.dataloader(args.base_dir + args.datapath)
 
-num_total = len(images)
-indices = list(range(num_total))
-split = int(np.floor(args.split * num_total))
+images, all_params, labels = lister.dataloader(args.base_dir + args.datapath)
+benign_images, malignant_images = images
+benign_params, malignant_params = all_params
+benign_labels, malignant_labels = labels
+
+print("Using %.2f of data for evaluation" % args.split)
+num_benign_total, num_malignant_total = len(benign_images), len(malignant_images)
+benign_indices, malignant_indices = list(range(num_benign_total)), list(range(num_malignant_total))
+benign_split, malignant_split = int(np.floor(args.split * num_benign_total)), int(np.floor(args.split * num_malignant_total))
 
 np.random.seed(3)
-np.random.shuffle(indices)
+np.random.shuffle(benign_indices)
+np.random.shuffle(malignant_indices)
 
-train_idx, test_idx = indices[split:], indices[:split]
+train_idx, test_idx = benign_indices[benign_split:], benign_indices[:benign_split]
+malignant_train_idx, malignant_test_idx = [num_benign_total + i for i in malignant_indices[malignant_split:]], [num_benign_total + i for i in malignant_indices[:malignant_split]]
+train_idx.extend(malignant_train_idx)
+test_idx.extend(malignant_test_idx)
+
+images[0].extend(images[1])
+all_params[0].extend(all_params[1])
+labels[0].extend(labels[1])
+images, all_params, labels = images[0], all_params[0], labels[0]
+
 train_images, train_params, train_labels = [images[i] for i in train_idx], [
     all_params[i] for i in train_idx], [labels[i] for i in train_idx]
 test_images, test_params, test_labels = [images[i] for i in test_idx], [
@@ -78,6 +91,8 @@ transform = transforms.Compose([
                          (0.2023, 0.1994, 0.2010)),
 ])
 
+print("Length of DataLoader: %d" % len(test_images))
+print(set(test_labels))
 TestImgLoader = torch.utils.data.DataLoader(
     DA.ImageFolder(test_images, test_params, test_labels,
                    False, transform=transform),
@@ -93,7 +108,7 @@ if args.cuda:
     likelihood = gpytorch.likelihoods.SoftmaxLikelihood(num_features=num_features, n_classes=2).cuda()
 
 
-state_file = args.base_dir + args.checkpoints + 'dkl_breakhis_%s_checkpoint_%d.dat' % (resnet_type.__name__, args.load)
+state_file = args.base_dir + args.checkpoints + 'dkl_breakhis_%s_checkpoint_%d_%d.dat' % (resnet_type.__name__, int(args.split * 100), args.load)
 if args.cuda:
     print("Using CUDA")
     state = torch.load(state_file)
@@ -105,7 +120,7 @@ likelihood.load_state_dict(state['likelihood'])
 def analyze(data):
     model.eval()
     likelihood.eval()
-    correct, tp, fp, tn, fn = 0, 0, 0, 0, 0
+    correct, tp, fp, tn, fn = 0., 0., 0., 0., 0.
     preds = torch.zeros(len(data.dataset))
     label_preds = torch.zeros(len(data.dataset))
     labels = torch.zeros(len(data.dataset))
@@ -116,18 +131,19 @@ def analyze(data):
             distr = model(image)
             output = likelihood(distr)
             pred = output.probs.argmax(1)
-            preds[i*args.batch_size:(i+1)*args.batch_size] = output.probs[:, 1]
-            labels[i*args.batch_size:(i+1)*args.batch_size] = label
-            label_preds[i*args.batch_size:(i+1)*args.batch_size] = pred
+            preds[i] = output.probs[:, 1]
+            labels[i] = label
+            label_preds[i] = pred
             label_comp = label.view_as(pred)
-            tp += (((pred == 1) + (label_comp == 1)) == 2).sum()
-            fp += (((pred == 1) + (label_comp == 0)) == 2).sum()
-            tn += (((pred == 0) + (label_comp == 0)) == 2).sum()
-            fn += (((pred == 0) + (label_comp == 1)) == 2).sum()
+            tp += float((((pred == 1) + (label_comp == 1)) == 2).sum())
+            fp += float((((pred == 1) + (label_comp == 0)) == 2).sum())
+            tn += float((((pred == 0) + (label_comp == 0)) == 2).sum())
+            fn += float((((pred == 0) + (label_comp == 1)) == 2).sum())
             correct += pred.eq(label.view_as(pred)).cpu().sum()
     print('Accuracy: {}/{} ({}%)'.format(correct, len(data.dataset), 100. * correct / float(len(data.dataset))))
     print('Sensitivity Values: TP {}, FP {}, TN {}, FN {}'.format(tp, fp, tn, fn))
     labels, preds = labels.cpu().numpy(), preds.cpu().numpy()
+    print('Precision: %0.3f, Recall: %0.3f' % (float(tp*1.0/(tp+fp)), float(tp*1.0/(tp+fn))))
     fpr, tpr, thresholds = roc_curve(labels, preds)
     auc = roc_auc_score(labels, preds)
     ap = average_precision_score(labels, preds)
@@ -135,10 +151,6 @@ def analyze(data):
 
 with gpytorch.settings.use_toeplitz(False), gpytorch.settings.max_preconditioner_size(0):
     fpr, tpr, thresholds, auc, ap  = analyze(TestImgLoader)
-    print(fpr)
-    print(tpr)
-    print(thresholds)
-    print(ap)
     print("Positive AUC: %0.4f" % auc)
     plt.figure()
     lw = 2